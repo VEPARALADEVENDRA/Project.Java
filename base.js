21fda6495df0099153eb15fbd709ab5ec546db8e
Deva <vdevendra746@gmail.com>
2025-06-28 16:48:00 +0530

    second  commit

--- base.js
+++ base.js
@@ -1,39 +1,83 @@
-// Question 1:
+  /*  Question 1:
 
 
-// // // let n= prompt("Enter the user name:");
-// // // function question1(userName)
-// // // {
-    
-// // //     console.log("Hello,"+ n +" ! Welcome to JavaScript");
-// // // }   
-// // // question1(n);
+    let n= prompt("Enter the user name:");
+    function question1(userName)
+
+        console.log("Hello,"+ n +" ! Welcome to JavaScript");
+    }   
+    question1(n);  */
 
 
-// Question 2:
-// // function square (num) {
-// //     return num * num;
-// // }
-// // console.log(square(5)); 
-
-// Question 3:
-
-// function addNumbers(num1, num2) {
-//     return num1 + num2;
-
-    
-// }
-// console.log(addNumbers(5, 10));
+    /*  Question 2:
+        function square (num) {
+        return num * num;
+        }
+        console.log(square(5)); */
 
 
-// Question 4:
+ /*   Question 3:
 
-// const a = function() {
-//     return "Hello, World!";
+    function addNumbers(num1, num2) {
+        return num1 + num2;
 
-// }
-// console.log(a());
+        
+    }
+    console.log(addNumbers(5, 10)); */
+
+
+  /*  Question 4:
+
+    const a = function() {
+        return "Hello, World!";
+
+    }
+    console.log(a()); */
 
 
 
-// Question 5:
+  /*  Question 5:
+    let a=Number(prompt("Enter a"));
+    let b=Number(prompt("Enter b"));
+
+    const add=function(a,b)
+    {
+        return a*b;
+    }
+    console.log(add(a,b));  */
+
+
+     /*   Question 6:
+        let a=Number(prompt("Enter a"));
+
+    const even=function(a)
+    {
+        return a%2==0;
+    }
+    console.log(even(a));   */
+
+
+     /*   Question 7:
+        let num=Number(prompt("Enter the Number"));
+
+    const square=(n)=>n*n;
+
+    console.log(square(num));   */
+
+
+
+     /*       Question 8:
+        let a=prompt("Enter the text");
+
+        const sayGood=a=>a+" !";
+
+        console.log(sayGood(a));  */
+
+
+    /*    Question 9:
+        let a=Number(prompt("Enter the a"));
+        let b=Number(prompt("Enter the b"));
+
+        const subtract=(a,b)=>a-b;
+
+        console.log(subtract(a,b));   */
