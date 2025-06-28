cd12e734e684d5913bb5f8971ee419a8d93e30d6
Deva <vdevendra746@gmail.com>
2025-06-28 16:27:59 +0530

    Initial commit

---
+++ base.js
@@ -0,0 +1,39 @@
+// Question 1:
+
+
+// // // let n= prompt("Enter the user name:");
+// // // function question1(userName)
+// // // {
+    
+// // //     console.log("Hello,"+ n +" ! Welcome to JavaScript");
+// // // }   
+// // // question1(n);
+
+
+// Question 2:
+// // function square (num) {
+// //     return num * num;
+// // }
+// // console.log(square(5)); 
+
+// Question 3:
+
+// function addNumbers(num1, num2) {
+//     return num1 + num2;
+
+    
+// }
+// console.log(addNumbers(5, 10));
+
+
+// Question 4:
+
+// const a = function() {
+//     return "Hello, World!";
+
+// }
+// console.log(a());
+
+
+
+// Question 5:
